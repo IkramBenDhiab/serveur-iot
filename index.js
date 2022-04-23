0d9e9f0862a93090e8cd0e1171d89f8b396a0e71
ikram2019 <76854531+ikram2000@users.noreply.github.com>
2022-04-23 11:57:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+const express = require ("express")
+var app = express()
+app.get("/",function(req,res){
+res.send("le serveur est en marche : )")
+})
+
+
+app.use(express.urlencoded({extended:true}))
+app.listen(process.env.PORT || 5000,()=>{
+    console.log('now listening for requests on port',process.env.PORT);})
